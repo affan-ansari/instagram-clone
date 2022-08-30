2e39695a69b11ca29a69bc4394d423322549219c
Affan Arif <muhammad.affan@devsinc.com>
2022-08-31 02:20:54 +0500

    Recreated schema
    * Deleted schema and recreated using migration
    * Posts table removed from schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 2022_08_30_122902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string "caption"
-    t.integer "likes", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

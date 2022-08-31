a5a570a4d1d576621587a4e5029c875ca8bbc569
Affan Arif <muhammad.affan@devsinc.com>
2022-08-31 16:20:19 +0500

    Post Create implementation
    * Create action functionality
    * Show action functionality
    * Strong Params functino
    * Form for new post
    * Post partial to DRY up code
    * Post migration generated
    * Schema changes for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,35 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
-  def update
-  end
+  def update; end
 
-  def destroy
-  end
+  def destroy; end
 
   def show
+    @post = Post.find(params[:id])
   end
 
-  def edit
+  def edit; end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption)
   end
 end

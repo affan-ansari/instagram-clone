1e1b8befa4b3eb99c38fb132bf3d8def398a6d80
Affan Arif <muhammad.affan@devsinc.com>
2022-09-06 22:31:26 +0500

    Posts Images validation
    * Check image presence
    * Check if file is an image(png, jpg, jpeg)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,26 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images
-  validate :validate_images
+  validate :validate_images_count, :validate_image_presence, :validate_image_type
 
   private
 
-  def validate_images
+  def validate_images_count
     errors.add(:images, 'Too many images') if images.length > 10
+  end
+
+  def validate_image_presence
+    errors.add(:image, 'No image attached') if images.attached? == false
+  end
+
+  def validate_image_type
+    return unless images.attached?
+
+    images.each do |image|
+      if %w[image/png image/jpg image/jpeg].include?(image.content_type) == false
+
+        errors.add(:image, 'File must be an of type jpg, jpeg or png')
+      end
+    end
   end
 end

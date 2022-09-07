927abd1ce22f3d5a3a747ee87b622beb58e960eb
Affan Arif <muhammad.affan@devsinc.com>
2022-09-07 23:39:01 +0500

    Changes to comments skeleton
    * Skeleton Views created (WIP)
    * Basic actions for comments controller
    * Relation of comment and post
    * Configured routes for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,29 @@
 class CommentsController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end

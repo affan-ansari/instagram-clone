acfca98a7c6a64711e8e54160f7825ce94d654dd
Affan Arif <muhammad.affan@devsinc.com>
2022-09-09 17:42:12 +0500

    Like and Dislike functionality
    * Create and destroy action for likes
    * like partial created
    * Likes count displayed

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,20 @@
 class LikesController < ApplicationController
+  def create
+    @like = current_user.likes.new(like_params)
+    flash[:alert] = @like.errors.full_messages.to_sentence unless @like.save
+    redirect_to post_path(@like.post)
+  end
+
+  def destroy
+    @like = current_user.likes.find(params[:id])
+    post = @like.post
+    @like.destroy
+    redirect_to post
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:post_id)
+  end
 end

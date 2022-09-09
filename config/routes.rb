1bb04a6ba82660f7e69dd509ec68843c4606f820
Affan Arif <muhammad.affan@devsinc.com>
2022-09-09 12:16:15 +0500

    changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, except: %i[show index]
   end
-<<<<<<< HEAD
   resources :stories, except: %i[edit update]
-=======
->>>>>>> a3fcf3cc92ac18753689a709219399f9ae7a750b
   resources :users, only: %i[show index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

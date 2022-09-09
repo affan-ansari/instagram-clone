d5bafd87c7ff0a1daa09333e0aba59716d34b0a0
Affan Arif <muhammad.affan@devsinc.com>
2022-09-09 14:13:23 +0500

    Edit comment functionality
    * Authorized user can edit comment
    * Display edit option for authorized users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,28 @@ class CommentsController < ApplicationController
     end
   end
 
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    if @comment.update(comment_params)
+      flash[:notice] = 'Comment was successfully updated'
+      redirect_to @post
+    else
+      flash[:alert] = 'Comment was not updated'
+      render 'edit'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    return unless authorize_user_for_edit?(@comment) == false
+
+    flash[:alert] = 'Not authorized'
+    redirect_to post_path(@post)
+  end
+
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
@@ -32,7 +54,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body, :user)
+    params.require(:comment).permit(:body)
   end
 
   def authorize_user_for_delete?(comment)

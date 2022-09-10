1858c98dce9a693a63df0219066a1dd0e5620e78
Affan Arif <muhammad.affan@devsinc.com>
2022-09-11 04:20:22 +0500

    Following validation
    * user cannot be self follower

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,5 +1,10 @@
 class Following < ApplicationRecord
   validates :user_id, uniqueness: { scope: :follower_id }
+  validate :user_can_not_be_self_follower
   belongs_to :user
   belongs_to :follower, class_name: 'User'
+
+  def user_can_not_be_self_follower
+    errors.add(:follower_id, 'User cannot be self-follower') if user_id == follower_id
+  end
 end

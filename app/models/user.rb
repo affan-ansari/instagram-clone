6a4f2a930690571d354d15c2fc57645435310a63
Affan Arif <muhammad.affan@devsinc.com>
2022-09-11 01:39:29 +0500

    Relationship of user and followings
    * User has many followings
    * User has many followers through followings

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,7 @@ class User < ApplicationRecord
   has_many :stories, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :followings, dependent: :destroy
+  has_many :followers, through: :followings
 end

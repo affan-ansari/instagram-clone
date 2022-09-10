9655157c8e30988c988ec4ad571e658370b092da
Affan Arif <muhammad.affan@devsinc.com>
2022-09-11 04:20:00 +0500

    Followings controller functionality
    * Create following functionality

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,2 +1,17 @@
 class FollowingsController < ApplicationController
+  def create
+    @following = current_user.followings.build(follower_id: params[:follower_id])
+    if @following.save
+      flash[:notice] = 'Successfully created Follow'
+    else
+      flash[:alert] = @following.errors.full_messages.to_sentence
+    end
+    redirect_to users_path
+  end
+
+  def destroy
+    puts 'Follow create action------------------------'
+    flash[:notice] = 'Successfully destroyed Follow'
+    redirect_to 'users'
+  end
 end

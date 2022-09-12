b45274e1d9a68747a5b847f9c412b29b6e6b464e
Affan Arif <muhammad.affan@devsinc.com>
2022-09-13 01:40:31 +0500

    Rescue pundit policy exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_search
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protected
 
   def configure_permitted_parameters
@@ -25,5 +27,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
+  end
+
+  def user_not_authorized
+    flash[:alert] = 'You are not authorized to perform this action'
+    redirect_to(request.referer || root_path)
   end
 end

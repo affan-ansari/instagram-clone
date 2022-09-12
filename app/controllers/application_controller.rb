035fe9e107e2ab5d275a604ec5f7d1beda577263
Affan Arif <muhammad.affan@devsinc.com>
2022-09-12 23:59:30 +0500

    Included pundit authorization module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_search

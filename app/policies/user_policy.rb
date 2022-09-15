5250a4e2a149bc53a49475edd852d1573cbcf5c9
Affan Arif <muhammad.affan@devsinc.com>
2022-09-16 03:10:30 +0500

    Changes to user policies
    * show followings of authenticated user

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -12,4 +12,8 @@ class UserPolicy
     following = accessed_user.followings.find_by(follower_id: @user.id, is_accepted: true)
     @accessed_user == @user || @accessed_user.is_public || !following.nil?
   end
+
+  def show_followings?
+    @accessed_user == @user
+  end
 end

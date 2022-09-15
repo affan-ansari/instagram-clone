d5e3c3d718a3cbd9bcc0e8870c6d3c9ebbb81eb6
Affan Arif <muhammad.affan@devsinc.com>
2022-09-15 12:56:40 +0500

    Accepted status added to followings
    * Created migration to add is_accepted column (boolean)
    * Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_13_081118) do
+ActiveRecord::Schema.define(version: 2022_09_15_073948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2022_09_13_081118) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "follower_id"
+    t.boolean "is_accepted", default: false, null: false
     t.index ["follower_id"], name: "index_followings_on_follower_id"
     t.index ["user_id", "follower_id"], name: "index_followings_on_user_id_and_follower_id", unique: true
     t.index ["user_id"], name: "index_followings_on_user_id"

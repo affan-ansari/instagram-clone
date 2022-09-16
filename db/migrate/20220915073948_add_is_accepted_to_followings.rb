297406eaad65b589dd0b2f58d17324be7e88f824
Affan Arif <muhammad.affan@devsinc.com>
2022-09-16 11:56:47 +0500

    Frozen string literal comment added

--- db/migrate/20220915073948_add_is_accepted_to_followings.rb
+++ db/migrate/20220915073948_add_is_accepted_to_followings.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddIsAcceptedToFollowings < ActiveRecord::Migration[5.2]
   def change
     add_column :followings, :is_accepted, :boolean, default: false, null: false

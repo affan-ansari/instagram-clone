6b0c6ef60dd5816b8fbce46ed32245133c6746cf
Affan Arif <muhammad.affan@devsinc.com>
2022-09-18 19:58:06 +0500

    Updated devise route
    * Included registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
   devise_scope :user do
     authenticated :user do
       root 'posts#index', as: :authenticated_root

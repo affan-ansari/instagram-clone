2a58226cfd655c698ed9ac773f874394df6383ca
Affan Arif <muhammad.affan@devsinc.com>
2022-09-19 12:47:32 +0500

    DRY up stories controller
    * before action callback to fetch story
    * Use destroy! for exception handling
    * Fetch stories of self and followed users only

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,10 +2,12 @@
 
 class StoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :fetch_story, only: %i[show destroy]
 
   def index
     followed_user_ids = Following.where(follower: current_user, is_accepted: true).pluck(:user_id)
-    @stories = Story.where(user: followed_user_ids).order('user_id, created_at').includes(:user)
+    @stories = Story.where(user: followed_user_ids)
+                    .or(Story.where(user: current_user)).order('user_id, created_at').includes(:user)
   end
 
   def new
@@ -25,17 +27,17 @@ class StoriesController < ApplicationController
   end
 
   def destroy
-    @story = Story.find(params[:id])
+    # @story = Story.find(params[:id])
     authorize @story
 
-    @story.destroy
+    @story.destroy!
 
     flash[:notice] = 'Story destroyed successfully'
     redirect_to stories_path
   end
 
   def show
-    @story = Story.find(params[:id])
+    # @story = Story.find(params[:id])
     authorize @story
   end
 
@@ -43,5 +45,9 @@ class StoriesController < ApplicationController
 
   def story_params
     params.require(:story).permit(:caption, :image)
+  end
+
+  def fetch_story
+    @story = Story.find(params[:id])
   end
 end

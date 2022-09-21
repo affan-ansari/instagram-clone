30cf12a39ae261c0e3e2cb6d33bd645ae7457655
Affan Arif <muhammad.affan@devsinc.com>
2022-09-21 11:30:24 +0500

    changes to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,33 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-user = User.new
-user.email = 'usama.manzoor@devsinc.com'
-user.encrypted_password = '$2a$12$ePQ961uLFdPshrK8ba229ugA.hRkB465we/vRmaFH1tBak5KCuUoO'
-user.name = 'Usama Manzoor'
-user.save!(validate: false)
+user1 = User.new
+user1.email = 'muhammad.affan@devsinc.com'
+user1.encrypted_password = '$2a$12$ePQ961uLFdPshrK8ba229ugA.hRkB465we/vRmaFH1tBak5KCuUoO'
+user1.name = 'Affan Devsinc'
+user1.save!(validate: false)
 
-user = User.new
-user.email = 'muhammad.affan@devsinc.com'
-user.encrypted_password = '$2a$12$ePQ961uLFdPshrK8ba229ugA.hRkB465we/vRmaFH1tBak5KCuUoO'
-user.name = 'Affan Arif'
-user.save!(validate: false)
+user2 = User.new
+user2.email = 'affanarif74@gmail.com'
+user2.encrypted_password = '$2a$12$ePQ961uLFdPshrK8ba229ugA.hRkB465we/vRmaFH1tBak5KCuUoO'
+user2.name = 'Affan Arif'
+user2.save!(validate: false)
 
-user = User.new
-user.email = 'talha.mustafa@devsinc.com'
-user.encrypted_password = '$2a$12$ePQ961uLFdPshrK8ba229ugA.hRkB465we/vRmaFH1tBak5KCuUoO'
-user.name = 'Talha Mustafa'
-user.save!(validate: false)
+story1 = Story.new
+story1.caption = "This is devsinc affan's story"
+story1.user = user1
+story1.image.attach(
+  filename: 'jBiNts3SmPyaRH315BoWE8DE.jpg',
+  io: URI.open('https://res.cloudinary.com/instagram-clone-affan/image/upload/v1663584876/jBiNts3SmPyaRH315BoWE8DE.jpg')
+)
+
+story1.save!
+
+story2 = Story.new
+story2.caption = "This is affan's personal story"
+story2.user = user2
+story2.image.attach(
+  filename: 'AG3RdJv9D16yVor89dZ9M6L6.jpg',
+  io: URI.open('https://res.cloudinary.com/instagram-clone-affan/image/upload/v1663583408/AG3RdJv9D16yVor89dZ9M6L6.jpg')
+)
+
+story2.save!

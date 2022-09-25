89dd4b3a518977ddf3ad2013046014cbc28071e1
Affan Arif <muhammad.affan@devsinc.com>
2022-09-25 20:11:09 +0500

    User model spec
    * Associations testing
    * Validation testing
    * Custom validation testing

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { is_expected.to have_many(:posts).class_name('Post').dependent(:destroy) }
+    it { is_expected.to have_many(:stories).class_name('Story').dependent(:destroy) }
+    it { is_expected.to have_many(:comments).class_name('Comment').dependent(:destroy) }
+    it { is_expected.to have_many(:likes).class_name('Like').dependent(:destroy) }
+    it { is_expected.to have_many(:followings).class_name('Following').dependent(:destroy) }
+    it { is_expected.to have_many(:followers).through(:followings) }
+
+    #     it {
+    #  expect(subject).to have_many(:inverse_followings).class_name('Following').with_foreign_key('follower_id').dependent(:destroy).inverse_of(:followings) }
+
+    # it { is_expected.to have_many(:follows).through(:inverse_followings) }
+    it { is_expected.to have_one_attached(:image) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe 'custom validations' do
+    let(:u1) { create(:user) }
+
+    it 'File is not an image of png, jpg, jpeg' do
+      u1.image.attach(
+        io: File.open(Rails.root.join('spec/fixtures/test.pdf')), filename: 'test.pdf'
+      )
+      expect(u1.valid?).to be(false)
+    end
+
+    it 'File is an image' do
+      u1.image.attach(
+        io: File.open(Rails.root.join('spec/fixtures/avatar.jpg')), filename: 'avatar.jpg'
+      )
+      expect(u1.valid?).to be(true)
+    end
+  end
+end
